946517b2ab97bf15304e6c53620adceac013b907
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-03 14:26:05 +0900

    user_idでuniqかける

--- db/schema.sql
+++ db/schema.sql
@@ -16,5 +16,6 @@ CREATE TABLE diary (
     `diary_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id` BIGINT UNSIGNED NOT NULL,
     `title` VARCHAR(256) NOT NULL,
-    PRIMARY KEY (diary_id)
+    PRIMARY KEY (diary_id),
+    UNIQUE KEY (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
