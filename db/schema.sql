1739d1a45e0b2836dd558bc7f3ea4a1a8cc2e24b
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-03 13:52:48 +0900

    PRIMARY KEY変更

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user (
     `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARBINARY(32) NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (user_id),
     UNIQUE KEY (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -9,12 +9,12 @@ CREATE TABLE entry (
     `entry_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(512) NOT NULL,
     `content` VARCHAR(512) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (entry_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE diary (
     `diary_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id` BIGINT UNSIGNED NOT NULL,
     `title` VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (diary_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
