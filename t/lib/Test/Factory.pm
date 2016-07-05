85b682ad2c6ad6d572ff1220832b666b58b1e5cd
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-05 20:34:45 +0900

    不必要なコメントアウトを消す

--- t/lib/Test/Factory.pm
+++ t/lib/Test/Factory.pm
@@ -13,11 +13,9 @@ our @EXPORT = qw(
      create_entry
 );
 
-#use Intern::Bookmark::Util;
 use Diary::Context;
 use Diary::Service::User;
 use Diary::Service::Diary;
-#use Entry;
 
 sub create_user {
     my %args = @_;
@@ -66,48 +64,5 @@ sub create_entry {
        path => $path,
    });
 }
-
-# sub create_entry {
-#     my %args = @_;
-#     my $url      = $args{url} // 'http://' . random_regex('\w{15}') . '.com/';
-#     my $title    = $args{title} // random_regex('\w{50}');
-#     my $created  = $args{created} // Intern::Bookmark::Util::now;
-#     my $updated  = $args{updated} // Intern::Bookmark::Util::now;
-
-#     my $c = Intern::Bookmark::Context->new;
-#     my $dbh = $c->dbh;
-#     $dbh->query(q[
-#         INSERT INTO entry (url, title, created, updated)
-#           VALUES (?)
-#     ], [ $url, $title, $created, $updated ]);
-
-#     return Intern::Bookmark::Service::Entry->find_entry_by_url($dbh, {
-#         url => $url
-#     });
-# }
-
-# sub create_bookmark {
-#     my %args = @_;
-#     my $user    = $args{user}    // create_user();
-#     my $entry   = $args{entry}   // create_entry();
-#     my $comment = $args{comment} // random_regex('\w{50}');
-#     my $created = $args{created} // Intern::Bookmark::Util::now;
-#     my $updated = $args{updated} // Intern::Bookmark::Util::now;
-
-#     my $c = Intern::Bookmark::Context->new;
-#     my $dbh = $c->dbh;
-#     $dbh->query(q[
-#         INSERT INTO bookmark (user_id, entry_id, comment, created, updated)
-#           VALUES (?)
-#     ], [ $user->user_id, $entry->entry_id, $comment, $created, $updated ]);
-
-#     my $bookmark = Intern::Bookmark::Service::Bookmark->find_bookmark_by_user_and_entry($dbh, {
-#         user => $user,
-#         entry => $entry
-#     });
-#     $bookmark->entry($entry);
-#     $bookmark->user($user);
-#     return $bookmark;
-# }
 
 1;
