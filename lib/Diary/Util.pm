141eaa500940222e03edc424e5dcfc25b1c9066b
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-06 23:39:00 +0900

    時間関係のutil関数群を追加

---
+++ lib/Diary/Util.pm
@@ -0,0 +1,25 @@
+package Diary::Util;
+use strict;
+use warnings;
+use utf8;
+
+use DateTime;
+use DateTime::Format::MySQL;
+
+use Diary::Config;
+
+sub now () {
+    my $now = DateTime->now(time_zone => config->param('db_timezone'));
+    $now->set_formatter( DateTime::Format::MySQL->new );
+    $now;
+}
+
+sub datetime_from_db ($) {
+    my $dt = DateTime::Format::MySQL->parse_datetime( shift );
+    $dt->set_time_zone(config->param('db_timezone'));
+    $dt->set_formatter( DateTime::Format::MySQL->new );
+    $dt;
+}
+
+1;
+__END__
