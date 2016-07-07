83638cd504a46124f4630d6748128ebd80a2b2c0
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-07 20:16:59 +0900

    インデント修正

--- lib/Diary/Engine/Entry.pm
+++ lib/Diary/Engine/Entry.pm
@@ -15,68 +15,72 @@ sub default {
     my $diary = Diary::Service::Diary->find_diary_by_user($c->dbh, {
         user => $user,
     });
-    my $entry = Diary::Service::Entry->find_entry_by_path($c->dbh, {
-        user    => $user,
+    my $entry = Diary::Service::Entry->find_entry_by_path( $c->dbh, {
+        user  => $user,
         diary => $diary,
-        path => $path,
-    });
-    $c->html('entry.html', {
-        entry    => $entry,
-    });
+        path  => $path,
+    } );
+    $c->html( 'entry.html', {
+        entry => $entry,
+    } );
 }
 
 sub add_get {
     my ($class, $c) = @_;
-    my $path = $c->req->parameters->{path};
+    my $title   = $c->req->string_param('title');
+    my $content = $c->req->string_param('content');
+    my $path    = $c->req->parameters->{path};
 
     my $entry;
     if ($path) {
-        my $user = $c->user;
-        my $diary = Diary::Service::Diary->find_diary_by_user($c->dbh, {
+        my $user  = $c->user;
+        my $diary = Diary::Service::Diary->find_diary_by_user( $c->dbh, {
             user => $user,
-        });
-        $entry = Diary::Service::Entry->find_entry_by_path($c->dbh, {
+        } );
+        $entry = Diary::Service::Entry->find_entry_by_path( $c->dbh, {
             user    => $user,
-            diary => $diary,
-            path => $path,
-        });
+            diary   => $diary,
+            title   => $title,
+            content => $content,
+            path    => $path,
+        } );
     }
-    $c->html('entry/add.html', {
-        entry    => $entry,
-    });
+    $c->html( 'entry/add.html', {
+        entry => $entry,
+    } );
 }
 
 sub add_post {
     my ($class, $c) = @_;
-
-    my $title = $c->req->string_param('title');
+    my $title   = $c->req->string_param('title');
     my $content = $c->req->string_param('content');
 
-    my $user = $c->user;
-    my $diary = Diary::Service::Diary->find_or_create_diary_by_user($c->dbh, {
-        user => $user,
+    my $user  = $c->user;
+    my $diary = Diary::Service::Diary->find_or_create_diary_by_user( $c->dbh, {
+        user  => $user,
         title => $user->name . 'の日記',
-    });
+    } );
 
-    my $entry = Diary::Service::Entry->find_entry_by_path($c->dbh, {
-        user    => $user,
+    my $entry = Diary::Service::Entry->find_entry_by_path( $c->dbh, {
+        user  => $user,
         diary => $diary,
-        path => $title,
-    });
+        path  => $title,
+    } );
     if ($entry) {
-        Diary::Service::Entry->update($c->dbh, {
+        Diary::Service::Entry->update( $c->dbh, {
+            user     => $user,
             entry_id => $entry->entry_id,
-            title => $title,
-            content => $content,
-        });
+            title    => $title,
+            content  => $content,
+        } );
     } else {
-        Diary::Service::Entry->create($c->dbh, {
+        Diary::Service::Entry->create( $c->dbh, {
             user    => $user,
-            diary => $diary,
-            title => $title,
+            diary   => $diary,
+            title   => $title,
             content => $content,
-            path => $title,
-        });
+            path    => $title,
+        } );
     }
     $c->res->redirect('/');
 }
