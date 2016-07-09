e69f77c0874125807fca1a7f4a831b96333ec01f
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-10 02:06:40 +0900

    タグ追加

---
+++ lib/Diary/Service/Tag.pm
@@ -0,0 +1,55 @@
+package Diary::Service::Tag;
+use strict;
+use warnings;
+use Carp qw(croak);
+use Diary::Model::Tag;
+use Diary::Util;
+
+sub find_tags_by_entry_id {
+    my ( $class, $db, $args ) = @_;
+    my $entry_id = $args->{entry_id} // croak 'entry_id required';
+
+    my $mapping = $db->select_all(
+        q[ SELECT * FROM entry_tag_map WHERE entry_id = ?],
+        $entry_id
+    ) or return;
+    my $tag_ids = [map {$_->{tag_id}} @$mapping];
+    my $tags = [map $class->find_tag_by_tag_id($db, {tag_id => $_}), @$tag_ids];
+    return $tags;
+}
+
+sub find_tag_by_tag_id {
+    my ( $class, $db, $args ) = @_;
+    my $tag_id = $args->{tag_id} // croak 'tag_id required';
+
+    my $row = $db->select_row(
+        q[ SELECT * FROM tag WHERE tag_id = ?],
+        $tag_id
+    ) or return;
+    return Diary::Model::Tag->new($row);
+}
+
+sub find_tag_by_name {
+    my ( $class, $db, $args ) = @_;
+    my $name = $args->{name} // croak 'name required';
+
+    my $row = $db->select_row(
+        q[ SELECT * FROM tag WHERE name = ?],
+        $name
+    ) or return;
+    return Diary::Model::Tag->new($row);
+}
+
+sub create {
+    my ( $class, $db, $args ) = @_;
+    my $entry = $args->{entry} // croak 'entry required';
+    my $entry_id = $entry->{entry_id};
+    my $name = $args->{tag} // croak 'tag required';
+    my $created = $args->{created} || Diary::Util::now;
+
+    $db->query( q[ INSERT INTO tag (name, created) VALUES (?) ], [$name, $created] );
+    my $tag = $class->find_tag_by_name($db, {name => $name});
+    $db->query( q[ INSERT INTO entry_tag_map (entry_id, tag_id) VALUES (?) ], [$entry_id, $tag->tag_id] );
+}
+
+1;
---
+++ t/service/tag.t
@@ -0,0 +1,48 @@
+package t::Service::Tag;
+
+use strict;
+use warnings;
+use utf8;
+use lib 't/lib';
+
+use parent qw(Test::Class);
+
+use Test::Diary;
+use Test::Mechanize;
+use Test::Factory;
+
+use Test::More;
+use Test::Deep;
+use Test::Exception;
+
+use String::Random qw(random_regex);
+
+use Diary::Context;
+use Diary::Service::Tag;
+
+sub _require : Tests(startup) {
+    my ($self) = @_;
+    require_ok 'Diary::Service::Tag';
+}
+
+sub create : Tests {
+    my ($self) = @_;
+    my $c = Diary::Context->new;
+
+    subtest 'タグ作成できる' => sub {
+        $c->dbh->query(q[ DELETE FROM entry_tag_map ]);
+        $c->dbh->query(q[ DELETE FROM tag ]);
+        my $entry = create_entry;
+        Diary::Service::Tag->create($c->dbh, {entry => $entry, tag => 'test1'});
+        Diary::Service::Tag->create($c->dbh, {entry => $entry, tag => 'test2'});
+        my $tags = Diary::Service::Tag->find_tags_by_entry_id($c->dbh, {entry_id => $entry->entry_id});
+        ok $tags;
+        is scalar @$tags, 2;
+        is $tags->[0]->name, 'test1';
+        is $tags->[1]->name, 'test2';
+    };
+}
+
+__PACKAGE__->runtests;
+
+1;
