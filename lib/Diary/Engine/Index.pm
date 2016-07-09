cb860eb60d8b3aa9b87341aa96f086898d8e6b97
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-09 11:36:37 +0900

    loginのgetとpostを実装

--- lib/Diary/Engine/Index.pm
+++ lib/Diary/Engine/Index.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use utf8;
 
+use Diary::Service::User;
 use Diary::Service::Diary;
 use Diary::Service::Entry;
 
@@ -27,6 +28,26 @@ sub default {
     });
 }
 
+sub login_get {
+    my ($class, $c) = @_;
+    print STDERR $class, "aaaa";
+    $c->html( 'login.html', {} );
+};
+
+sub login_post {
+    my ($class, $c) = @_;
+    my $name = $c->req->string_param('name');
+    my $user = Diary::Service::User->find_or_create_user_by_name($c->dbh, {
+        name => $name,
+    });
+
+    # リダイレクトするので、リクエストが終わってから入れ替える
+    # https://metacpan.org/pod/Plack::Middleware::Session#PLACK-REQUEST-OPTIONS
+    $c->request->session_options->{late_store}++;
+
+    $c->session->set($c->session->id, $user->name);
+    $c->res->redirect('/');
+}
 
 sub logout_post {
     my ($class, $c) = @_;
