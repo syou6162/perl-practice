27a2c67a6d41de5d81dd380478bd01f8e08ef8fc
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-13 13:34:23 +0900

    ord関数

--- snippets/unicode_codepoint.pl
+++ snippets/unicode_codepoint.pl
@@ -5,6 +5,7 @@ use Encode;
 use Data::Dumper;
 
 my $str = 'あ';
+printf ord($str); # ord関数で文字のコードポイントを知ることができる。結果は123543042
 printf "%x\n", ord($str); # 16進数として表示
 print encode_utf8 "\x{3042}\n"; # utf8で表示
 print Dumper $str;
