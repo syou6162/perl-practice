0cad3a9a312da7226ed7a2f24a7aa2b0a9172625
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-13 13:33:21 +0900

    dumperの例も追加

--- snippets/unicode_codepoint.pl
+++ snippets/unicode_codepoint.pl
@@ -1,7 +1,10 @@
 #!/usr/bin/env perl
+# http://d.hatena.ne.jp/perlcodesample/20130610/1371002107
 use utf8;
 use Encode;
+use Data::Dumper;
 
 my $str = 'あ';
 printf "%x\n", ord($str); # 16進数として表示
 print encode_utf8 "\x{3042}\n"; # utf8で表示
+print Dumper $str;
