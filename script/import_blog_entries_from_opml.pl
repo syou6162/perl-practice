8cec8b6c7ed59c0307c9f999847bd6f750e85828
Yasuhisa Yoshida <syou6162@gmail.com>
2016-07-16 09:03:58 +0900

    dbに日記を保存する

--- script/import_blog_entries_from_opml.pl
+++ script/import_blog_entries_from_opml.pl
@@ -1,14 +1,23 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use lib 'lib';
 use utf8;
 use Encode;
 use Carp qw(croak);
 
 use WWW::Mechanize;
 use XML::Simple;
+use XML::Feed;
 
 use Data::Dumper;
+
+use Diary::Service::User;
+use Diary::Service::Diary;
+use Diary::Service::Entry;
+use Diary::Service::Tag;
+use Diary::Context;
+use Diary::Config;
 
 sub is_rss_info {
     my $info = shift;
@@ -54,9 +63,49 @@ sub get_blogs_meta_info {
     return $outlines;
 }
 
+sub parse_rss {
+    my $content = shift;
+    my $data = XML::Feed->parse(\$content) or die XML::Feed->errstr;
+    my $user = $data->author || $data->title;
+    my $result = {
+        title => $data->title,
+        user => $user,
+        entries => [
+            map {
+                my $entry = $_;
+                {
+                    user  => $user,
+                        title => $entry->title,
+                        content => $entry->content->body,
+                        created => $entry->modified,
+                    }
+            } $data->entries
+        ],
+    };
+    return $result;
+}
+
+BEGIN { $ENV{INTERN_BOOKMARK_ENV} = 'local' };
+my $context = Diary::Context->from_env(config);
 my $mech = WWW::Mechanize->new( autocheck => 0 );
 my $blogs_meta_info = get_blogs_meta_info('opml.xml');
+
 foreach my $blog_meta_info (@$blogs_meta_info) {
     $mech->get( $blog_meta_info->{xmlUrl} );
-    print $mech->content if $mech->status eq '200';
+    my $blog;
+    if ($mech->status eq '200') {
+        eval { $blog = parse_rss $mech->content};
+        next unless $blog; # skip invalid rss format
+    } else {
+        next;
+    }
+    my $user = Diary::Service::User->find_or_create_user_by_name( $context->dbh, { name => $blog->{user} } );
+    my $diary = Diary::Service::Diary->find_or_create_diary_by_user( $context->dbh, { user => $user } );
+    for my $entry (@{$blog->{entries}}) {
+        my $hash = $entry;
+        $hash->{user} = $user;
+        $hash->{diary} = $diary;
+        my $entry = Diary::Service::Entry->create( $context->dbh, $hash );
+        sleep 1;
+    }
 }
