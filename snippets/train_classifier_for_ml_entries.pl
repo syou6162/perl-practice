9af6a149152507c87f1586f2586617654cd777b0
Yasuhisa Yoshida <syou6162@gmail.com>
2016-11-14 19:12:05 +0900

    テストデータ分類してslackに投げるところまで

--- snippets/train_classifier_for_ml_entries.pl
+++ snippets/train_classifier_for_ml_entries.pl
@@ -1,9 +1,14 @@
 use strict;
 use warnings;
 
+use utf8;
 use Encode;
 use Text::MeCab;
 use Algorithm::LibLinear;
+use LWP::Simple;
+use Clone qw(clone);
+use List::UtilsBy qw( nsort_by rev_nsort_by uniq_by );
+use WebService::Slack::IncomingWebHook;
 use v5.10;
 
 sub get_feature_id {
@@ -16,13 +21,6 @@ sub get_feature_id {
         $feature2id->{$feature} = 2 + scalar keys $feature2id;
     }
 };
-
-my $feature2id = {};
-
-say get_feature_id($feature2id, "a", 0);
-say get_feature_id($feature2id, "a", 0);
-say get_feature_id($feature2id, "b", 0);
-say get_feature_id($feature2id, "c", 1);
 
 sub get_words_from_text {
     my ($mecab, $text) = @_;
@@ -48,21 +46,26 @@ sub get_feature_vector {
     return $result;
 }
 
-my $mecab = Text::MeCab->new();
-my $text = "日本語の形態素解析のテストを行ってみます";
-
-use Data::Dumper; warn Dumper get_feature_vector($mecab, $feature2id, $text, 0);
-
 sub parse_line {
     my $line = shift;
     my ($url, $label, $title) = split /, /, $line, 3;
     return {
         url => $url,
-        label => $label, #$label ? 1 : -1,
+        label => $label ? 1 : -1,
         title => $title
     }
 }
 
+sub get_title {
+    my $url = shift;
+    my $html = get($url);
+    my $title = "";
+    if ($html && $html =~ m{<TITLE>(.*?)</TITLE>}gism) {
+        $title = $1;
+        $title =~ s/\n/ /g;
+    }
+    return $title;
+}
 
 sub make_training_example {
     my ($mecab, $feature2id, $line) = @_;
@@ -72,9 +75,17 @@ sub make_training_example {
     return $result;
 }
 
-my $test_line = "http://afurotti.hatenablog.com/entry/2016/08/16/213611, 0, 写真の引っ越しができるようになったのね！ - にゃじら的生活＋あるふぁ 猫ふんじゃった";
-
-# use Data::Dumper; warn Dumper make_training_example($mecab, $feature2id, $test_line);
+sub make_test_example {
+    my ($mecab, $feature2id, $url) = @_;
+    my $title = get_title($url);
+    my $feature = get_feature_vector($mecab, $feature2id, $title, 1);
+    my $result = {
+        url => $url,
+        title => $title,
+        feature => $feature
+    };
+    return $result;
+}
 
 sub read_training_data {
     my ($mecab, $feature2id, $filename) = @_;
@@ -89,43 +100,62 @@ sub read_training_data {
     $train_data;
 }
 
+my $feature2id = {};
+my $mecab = Text::MeCab->new();
 
 my $org_training_data = read_training_data($mecab, $feature2id, "data.txt");
-
-use Clone qw(clone);
-
-my $tmp = [@{clone($org_training_data)}[0..2000]];
-for my $example (@$tmp) {
-    use Data::Dumper; warn Dumper $example->{label};
+my $tmp_train = [@{clone($org_training_data)}];
+for my $example (@$tmp_train) {
     delete $example->{title};
     delete $example->{url};
 }
-
 my $data_set = Algorithm::LibLinear::DataSet->new(
-    data_set => $tmp
+    data_set => $tmp_train
 );
-
-# use Data::Dumper; warn Dumper $org_training_data;
-# use Data::Dumper; warn Dumper $data_set;
 
 my $learner = Algorithm::LibLinear->new(
     cost => 1.0,  # ペナルティコスト
-    epsilon => 0.1,  # 収束判定
+    epsilon => 0.0001,  # 収束判定
     solver => 'L2R_L2LOSS_SVC_DUAL',  # 分類器の学習に使うソルバ
 );
-
 my $classifier = $learner->train(data_set => $data_set);
 
-my $accuracy = $learner->cross_validation(
-    data_set => $data_set,
-    num_folds => 5,
+# for my $example (@$org_training_data) {
+#     my $score = $classifier->{raw_model}->predict_values($example->{feature})->[0];
+#     $example->{score} = $score;
+# }
+# for my $example (uniq_by {$_->{title}} uniq_by {$_->{url}} nsort_by {abs($_->{score})} @$org_training_data) {
+#     say $example->{score} . " " . $example->{title};
+# }
+
+my $test_examples = [];
+
+while (<STDIN>){
+    chomp($_);
+    push @$test_examples, make_test_example($mecab, $feature2id, $_);
+}
+
+for my $example (@$test_examples) {
+    my $score = $classifier->{raw_model}->predict_values($example->{feature})->[0];
+    $example->{score} = $score;
+}
+
+my $client = WebService::Slack::IncomingWebHook->new(
+    webhook_url => $ENV{ML_STUDY_WEBHOOK_URL}
 );
 
-say $accuracy;
+my $spreadsheet_url = $ENV{ML_STUDY_SPREADSHEET_URL};
+$client->post(
+    username   => 'ML君',
+    text       => "ml-study向けにこんなエントリはどうですか?教師データを追加/編集したい場合は<$spreadsheet_url|こちら>をどうぞ!",
+);
 
-use Data::Dumper; warn Dumper $tmp->[0];
-use Data::Dumper; warn Dumper $classifier->{raw_model}->predict($tmp->[0]);
-use Data::Dumper; warn Dumper $classifier->predict(feature => $tmp->[1]->{feature});
-use Data::Dumper; warn Dumper $classifier->{raw_model}->predict_values($tmp->[1]->{feature});
-use Data::Dumper; warn Dumper $classifier->{raw_model}->num_classes;
-# use Data::Dumper; warn say Dumper $classifier->predict_values($tmp->[0]->{feature});
+for my $example (uniq_by {$_->{title}} uniq_by {$_->{url}} rev_nsort_by {$_->{score}} @$test_examples) {
+    if ($example->{score} > 0) {
+        $client->post(
+            username   => 'ML君',
+            text       => "<$example->{url}|$example->{title}>",
+        );
+        warn say encode_utf8 $example->{score} . " " . $example->{title};
+    }
+}
